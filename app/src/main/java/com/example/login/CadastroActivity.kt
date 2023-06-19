a4697d1c5dce6ccd5b6f1882aac7dfbc699e1b05
phtrebil <phptrebil@gmail.com>
2023-06-19 14:41:28 -0300

    criando layout de login e de cadastro

--- app/src/main/java/com/example/login/CadastroActivity.kt
+++ app/src/main/java/com/example/login/CadastroActivity.kt
@@ -3,10 +3,9 @@ package com.example.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_cadastro)
     }
 }
-
