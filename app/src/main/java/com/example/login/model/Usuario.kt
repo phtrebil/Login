d2cb1c1660480f30652805c19a3ccd1df96ace80
phtrebil <phptrebil@gmail.com>
2023-06-20 16:20:13 -0300

    criando classe de usuario

---
+++ app/src/main/java/com/example/login/model/Usuario.kt
@@ -0,0 +1,9 @@
+package com.example.login.model
+
+class Usuario(
+    private val usuario: String,
+    private val nome: String,
+    private val email: String,
+    private val senha: String
+) {
+}
