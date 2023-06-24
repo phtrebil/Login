3c1567c9e67406b975db8b6d84fa1cfac03a84e1
phtrebil <phptrebil@gmail.com>
2023-06-24 07:49:27 -0300

    implementando classe de cadastro para salvar usuário

--- app/src/main/java/com/example/login/CadastroActivity.kt
+++ app/src/main/java/com/example/login/CadastroActivity.kt
@@ -2,10 +2,54 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.example.login.database.AppDatabase
+import com.example.login.databinding.ActivityCadastroBinding
+import com.example.login.model.Usuario
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class CadastroActivity : AppCompatActivity() {
+
+    private val binding by lazy{
+        ActivityCadastroBinding.inflate(layoutInflater)
+    }
+
+    private val dataBase by lazy {
+        AppDatabase.instancia(this).userDao()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro)
+        setContentView(binding.root)
+
+        configuraBotaoDeCadastro()
+
+
+    }
+
+    private fun configuraBotaoDeCadastro() {
+        binding.buttonCadastrar.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.IO) {
+                dataBase.insertAll(salvaUsuario())
+            }
+            finish()
+        }
+    }
+
+    private fun salvaUsuario(): Usuario{
+        val nome = binding.editTextNome.text.toString()
+        val user = binding.editTextNomeUsuario.text.toString()
+        val senha = binding.editTextSenha.text.toString()
+        val email = binding.editTextEmail.text.toString()
+
+        return Usuario(
+            nome = nome,
+            usuario = user,
+            email = email,
+            senha = senha
+        )
+
+
     }
 }
--- app/src/main/java/com/example/login/LoginActivity.kt
+++ app/src/main/java/com/example/login/LoginActivity.kt
@@ -3,12 +3,17 @@ package com.example.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.login.database.AppDatabase
 import com.example.login.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val dataBase by lazy {
+        AppDatabase.instancia(this).userDao()
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
